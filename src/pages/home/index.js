e071265958b8f6c5e295e1fef0c2ba4de84ba76c
Kumar Satwik <kumar.satwik@publicissapient.com>
2026-05-01 00:33:09 +0530

    setting up funmode settings

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,9 +7,16 @@ import {
   ButtonContainer,
   BodyContainer,
 } from "./index.style";
+import FunModeSettings from "../../components/funModeSettings/funModeSettings.js";
+import { useState } from "react";
+import useSoundManager from "../../hooks/useSoundManager";
+
 
 const Home = () => {
   const navigate = useNavigate();
+  const [showFunModeSettings,setShowFunModeSettings]=useState('false')
+  const soundManager = useSoundManager();
+
 
   return (
     <PageContainer>
@@ -23,8 +30,34 @@ const Home = () => {
         <CustomButton onClick={() => navigate("/play-online")}>
           Play Online
         </CustomButton>
+        <CustomButton onClick={() => (true)}>
+          Fun Mode Settings
+        </CustomButton>
       </ButtonContainer>
+      {showFunModeSettings && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: "rgba(0,0,0,0.8)",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            zIndex: 1000,
+          }}
+        >
+          <FunModeSettings
+            soundManager={soundManager}
+            onClose={() => setShowFunModeSettings(false)}
+          />
+        </div>
+      )}
     </PageContainer>
+
+    
   );
 };
 
